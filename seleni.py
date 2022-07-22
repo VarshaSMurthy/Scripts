8c10e1118c155ad2aca5576d4113ad1443399c65
VarshaSMurthy <73743263+VarshaSMurthy@users.noreply.github.com>
2022-07-22 16:31:32 +0530

    Update seleni.py

--- seleni.py
+++ seleni.py
@@ -3,12 +3,12 @@ from selenium import webdriver
 import unittest
 class Test(unittest.TestCase):
 	def testName(self):
-		self.driver=webdriver.Firefox(executable_path="/home/vss9kor/Documents/geckodriver")
-		self.driver.get("https://bzo.bosch.com/bzo/en/start_page.html")
+		self.driver=webdriver.Firefox(executable_path="/home/Documents/geckodriver")
+		self.driver.get("https//:google.com")
 #	def testName(self):
 		title=self.driver.title
-#		self.assertEqual("Bosch-Zünder Online",title,"error loading page")
-		self.assertEqual("Bosch-Zünder Online",title,"error loading page")
+#		self.assertEqual("Google",title,"error loading page")
+		self.assertEqual("Google",title,"error loading page")
 		print(title)
 		self.driver.close()
 if __name__=="__main__":
